ff724659af3f52a6c871e8111d64303ec16a9b8b
Hare Sudhan Muthusamy <haresudhan@zoho.com>
2020-04-20 22:49:11 -0700

    lab4 changed

--- lab4.py
+++ lab4.py
@@ -3,7 +3,7 @@ import base64
 from statistics import mean
 from xor import *
 import itertools
-
+import sys
 
 def normalized_edit_distance(x, k):
     blocks = [x[i:i+k] for i in range(0, len(x), k)][0:4]
@@ -18,10 +18,16 @@ def break_repeating_xor(x, k):
     key = [XORCipher(i).bruteforce().key for i in transposedBlocks]
     return bytes(key)
 
-
-with open("part4.txt","r") as f:
-    cipher_text = f.read()
-    cipher_text = base64.b64decode(cipher_text)
-    key_length = min(range(2, 50), key=lambda k: normalized_edit_distance(cipher_text, k))
-    cipher_key = break_repeating_xor(cipher_text,key_length)
-    print(XORCipher.decode(cipher_text,cipher_key))
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        try:
+            with open(sys.argv[1],"r") as f:
+                cipher_text = f.read()
+                cipher_text = base64.b64decode(cipher_text)
+                key_length = min(range(2, 50), key=lambda k: normalized_edit_distance(cipher_text, k))
+                cipher_key = break_repeating_xor(cipher_text,key_length)
+                print(XORCipher.decode(cipher_text,cipher_key))
+        except FileNotFoundError:
+            print("File Not Found.")
+    else:
+        print("Usage: python3 {} <cipher-file>".format(sys.argv[0]))
